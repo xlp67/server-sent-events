b5d510272be72cfa0b54488e927ec3087f675ef0
Thiago Saraiva <thiiagowilliam@gmail.com>
2023-11-29 14:55:19 -0300

    add write model

--- main.go
+++ main.go
@@ -5,9 +5,10 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 )
 
-func ResponseSSE(channel chan int, w http.ResponseWriter, r *http.Request) {
+func ResponseSSE(channel chan rune, w http.ResponseWriter, r *http.Request) {
 	if r.Header.Get("Accept") != "text/event-stream" {
 		http.Error(w, "O cliente não suporta SSE", http.StatusBadRequest)
 		return
@@ -17,31 +18,38 @@ func ResponseSSE(channel chan int, w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Cache-Control", "no-cache")
 	w.Header().Set("Connection", "keep-alive")
 
-	for i := range channel {
-		message := fmt.Sprintf("%d", i)
+	for char := range channel {
+		message := fmt.Sprintf("%c", char)
 		fmt.Fprintf(w, "data: %s\n\n", message)
 		w.(http.Flusher).Flush()
 	}
 }
 
-func ProcessData(channel chan int, data int) {
-	for i := 0; i < data; i++ {
-		channel <- i
+func ReadFile(channel chan rune, path string) {
+	content, err := os.ReadFile(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, char := range string(content) {
+		channel <- char
 	}
 	close(channel)
 }
 
 func Template(w http.ResponseWriter, r *http.Request) {
 	tpl, err := template.ParseFiles("index.html")
-	if err != nil {log.Fatal("erro no parse no template")}
+	if err != nil {
+		log.Fatal("erro no parse no template")
+	}
 	tpl.Execute(w, nil)
 }
+
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/partial-response", func(w http.ResponseWriter, r *http.Request) {
-		ch := make(chan int)
+		ch := make(chan rune)
 		go ResponseSSE(ch, w, r)
-		ProcessData(ch, 10)
+		ReadFile(ch, "../resolve-ai/main.go")
 	})
 	mux.HandleFunc("/", Template)
 	http.ListenAndServe(":8080", mux)
