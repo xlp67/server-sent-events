526a4216230167b85097435cca81fe339d7018df
Thiago Saraiva <thiiagowilliam@gmail.com>
2023-11-30 15:05:14 -0300

    consumindo []bytes e enviando para o sse

--- sse/sse.go
+++ sse/sse.go
@@ -6,18 +6,17 @@ import (
 	"sse/file"
 )
 func ResponseSSE(filePath string, w http.ResponseWriter, r *http.Request) {
-	if r.Header.Get("Accept") != "text/event-stream" {
+	if r.Header.Get("Accept") != "text/event-stream"{
 		http.Error(w, "O cliente não suporta SSE", http.StatusBadRequest)
 		return
 	}
-	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Content-Type", "text/event-stream; charset=utf-8")
 	w.Header().Set("Cache-Control", "no-cache")
 	w.Header().Set("Connection", "keep-alive")
-
 	bytesData := file.ReadFile(filePath)
-
 	for i := 0; i < len(bytesData); i ++ {
 		fmt.Fprintf(w, "data: %x\r\n\r\n", bytesData[i])
+		fmt.Println(bytesData[i])
 		w.(http.Flusher).Flush()
 	}
 }
